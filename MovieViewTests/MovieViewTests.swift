b4f658c986f50df8fbb19426a37578e41970ea75
Francois Denis <francois.denis@gmail.com>
2025-01-11 13:46:01 +0100

    Initial Commit

---
+++ MovieViewTests/MovieViewTests.swift
@@ -0,0 +1,16 @@
+//
+//  MovieViewTests.swift
+//  MovieViewTests
+//
+//  Created by Francois on 11/01/2025.
+//
+
+import Testing
+
+struct MovieViewTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
